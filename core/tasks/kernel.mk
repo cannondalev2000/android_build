ae47c4574948afa4413736960012f7b1714fec65
Chet Kener <Cl3Kener@gmail.com>
2014-12-03 19:58:15 -0500

    Build: Add Custom Toolchain Support
    * Many legacy devices don't support 4.8 so we need a 4.7 option and many of the newer devices do better either Linaro 4.8 or SM 4.8-4.10 so this just makes sense. Same flag as KitKat Liquid.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Change-Id: Ibf5a1204392214244b93316cc05bc715bf3b81bf
    
    Conflicts:
    	core/tasks/kernel.mk

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -160,6 +160,16 @@ ifeq ($(TARGET_ARCH),arm)
     else
         ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
     endif
+
+    ifneq ($(TARGET_GCC_VERSION_ARM),)
+      ifeq ($(HOST_OS),darwin)
+        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/arm/arm-eabi-$(TARGET_GCC_VERSION_ARM)/bin/arm-eabi-"
+      else
+        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-$(TARGET_GCC_VERSION_ARM)/bin/arm-eabi-"
+      endif
+    else
+      ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+    endif
     ccache = 
 endif
 
